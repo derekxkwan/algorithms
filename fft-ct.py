328ec0987c21d2dc6513a33f4749f6ba69833427
Derek Kwan <derek.x.kwan@gmail.com>
2016-02-07 00:22:18 -0800

    added ifft

--- fft-ct.py
+++ fft-ct.py
@@ -17,3 +17,22 @@ def fft(x):
         XkpNd2[k] = E[k] - oMult
     X = Xk + XkpNd2
     return X
+
+def ifft(X):
+    #must normalize by 1/N at end
+    N = len(X)
+    if N == 1:
+        return X
+    xk = [0 for x in range(N//2)]
+    xkpNd2 = [0 for x in range(N//2)]
+    even = X[0::2]
+    odd = X[1::2]
+    E = ifft(even)
+    O = ifft(odd)
+
+    for k in range(N//2):
+        oMult = exp(2j*pi*k/N)*O[k]
+        xk[k] = (E[k] + oMult)
+        xkpNd2[k] = (E[k] - oMult)
+    x = xk + xkpNd2
+    return x
